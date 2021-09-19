e019944c7e6c35cd803551641fb4d239d2ed1f6a
Oscar Barajas <oscar@gndx.dev>
2021-09-19 17:35:01 -0500

    :fire: [FIX] PWA

--- next.config.js
+++ next.config.js
@@ -1,64 +1,14 @@
-const path = require('path');
-const compose = require('next-compose')
-const withPWA = require('next-pwa');
-const runtimeCaching = require('next-pwa/cache');
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    mode: 'production',
+    disable: process.env.NODE_ENV === 'development',
+  },
+  images: {
+    domains: ['arepa.s3.amazonaws.com'],
+  },
 });
-
-module.exports = compose([
-  [withBundleAnalyzer, {
-    images: {
-      domains: ['arepa.s3.amazonaws.com'],
-    },
-    reactStrictMode: true,
-    pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-    eslint: {
-      dirs: ['pages', 'components', 'lib', 'layouts', 'scripts'],
-    },
-    experimental: { esmExternals: true },
-  }],
-  [withPWA, {
-    pwa: {
-      dest: 'public',
-      runtimeCaching,
-      disable: 'production'
-    },
-    webpack: (config) => {
-      config.resolve.modules.push(path.resolve('./'));
-      return config;
-    }
-  }],
-  {
-    webpack: (config, { dev, isServer }) => {
-      config.module.rules.push({
-        test: /\.(png|jpe?g|gif|mp4)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              publicPath: '/_next',
-              name: 'static/media/[name].[hash].[ext]',
-            },
-          },
-        ],
-      })
-
-      config.resolve.modules.push(path.resolve('./'));
-
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      })
-
-      if (!dev && !isServer) {
-        Object.assign(config.resolve.alias, {
-          react: 'preact/compat',
-          'react-dom/test-utils': 'preact/test-utils',
-          'react-dom': 'preact/compat',
-        })
-      }
-      return config
-    },
-  }
-]);
