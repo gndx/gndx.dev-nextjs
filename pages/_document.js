abcc692cd2034f4fb09a4e75433101365e5ebce5
Oscar Barajas Tavares <oscarbarajastavares@Oscars-MacBook-Pro.local>
2021-10-06 19:54:34 -0500

    :art: [ADD] OG image

--- pages/_document.js
+++ pages/_document.js
@@ -8,22 +8,42 @@ class MyDocument extends Document {
           <link data-react-helmet="true" href="favicon.png" rel="icon" type="image/png" />
           <link data-react-helmet="true" rel="manifest" href="/manifest.json" />
           <meta data-react-helmet="true" name="google" content="notranslate" />
-          <meta data-react-helmet="true" name="description"
-            content="Foundation Layer at @platzi - Microsoft MVP - Lead at Developer Circles from Facebook - I teach React &amp;amp; Svelte." />
+          <meta
+            data-react-helmet="true"
+            name="description"
+            content="Foundation Layer at @platzi - Microsoft MVP - Lead at Developer Circles from Facebook - I teach React &amp;amp; Svelte."
+          />
           <meta data-react-helmet="true" content="#333333" name="theme-color" />
-          <meta data-react-helmet="true" name="description"
-            content="Foundation Layer at @platzi - Microsoft MVP - Lead at Developer Circles from Facebook - I teach React &amp;amp; Svelte." />
+          <meta
+            data-react-helmet="true"
+            name="description"
+            content="Foundation Layer at @platzi - Microsoft MVP - Lead at Developer Circles from Facebook - I teach React &amp;amp; Svelte."
+          />
           <meta data-react-helmet="true" name="twitter:card" content="summary_large_image" />
           <meta data-react-helmet="true" name="twitter:site" content="@gndx" />
           <meta data-react-helmet="true" name="twitter:creator" content="@gndx" />
           <meta data-react-helmet="true" name="twitter:title" content="Oscar Barajas Tavares" />
-          <meta data-react-helmet="true" name="twitter:description"
-            content="Foundation Layer at @platzi - Microsoft MVP - Lead at Developer Circles from Facebook - I teach React &amp;amp; Svelte." />
-          <meta data-react-helmet="true" name="twitter:image" content="https://arepa.s3.amazonaws.com/og-gndx.png" />
+          <meta
+            data-react-helmet="true"
+            name="twitter:description"
+            content="Foundation Layer at @platzi - Microsoft MVP - Lead at Developer Circles from Facebook - I teach React &amp;amp; Svelte."
+          />
+          <meta
+            data-react-helmet="true"
+            name="twitter:image"
+            content="https://arepa.s3.amazonaws.com/og-gndx-dev-avatar.png"
+          />
           <meta data-react-helmet="true" property="og:title" content="Oscar Barajas Tavares" />
-          <meta data-react-helmet="true" property="og:description"
-            content="Foundation Layer at @platzi - Microsoft MVP - Lead at Developer Circles from Facebook - I teach React &amp;amp; Svelte." />
-          <meta data-react-helmet="true" property="og:image" content="https://arepa.s3.amazonaws.com/og-gndx.png" />
+          <meta
+            data-react-helmet="true"
+            property="og:description"
+            content="Foundation Layer at @platzi - Microsoft MVP - Lead at Developer Circles from Facebook - I teach React &amp;amp; Svelte."
+          />
+          <meta
+            data-react-helmet="true"
+            property="og:image"
+            content="https://arepa.s3.amazonaws.com/og-gndx-dev-avatar.png"
+          />
           <meta data-react-helmet="true" property="og:url" content="https://gndx.dev/" />
           <meta data-react-helmet="true" property="og:site_name" content="Oscar Barajas Tavares" />
           <meta data-react-helmet="true" property="og:locale" content="es_ES" />
