3b3df25f3474dd097e568249f2f3ce36c080a587
Oscar Barajas <oscar@gndx.dev>
2022-07-17 21:48:58 -0500

    :fire: [UPDATE] Delete Web3 Integration

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,21 +7,16 @@ import Head from 'next/head'
 import Analytics from 'src/components/analytics'
 import LayoutWrapper from 'src/components/LayoutWrapper'
 
-import { Web3ReactProvider } from "@web3-react/core";
-import { getLibrary } from "../config/web3";
-
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
-      <Web3ReactProvider getLibrary={getLibrary}>
-        <Head>
-          <meta content="width=device-width, initial-scale=1" name="viewport" />
-        </Head>
-        <Analytics />
-        <LayoutWrapper>
-          <Component {...pageProps} />
-        </LayoutWrapper>
-      </Web3ReactProvider>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      <Analytics />
+      <LayoutWrapper>
+        <Component {...pageProps} />
+      </LayoutWrapper>
     </ThemeProvider>
   )
 }
